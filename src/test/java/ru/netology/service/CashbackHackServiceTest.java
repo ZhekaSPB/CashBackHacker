50e86be56628b10d0704c35c9076db31efce493a
ZhekaSPB <23121986@list.ru>
2023-08-18 03:58:09 +0300

    junit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,50 @@
+package ru.netology.service;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void testBeforeLimitRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testLimitRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testAboveLimitRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+
+        int expected = 800;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void mustEqualsOne() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+
+}
